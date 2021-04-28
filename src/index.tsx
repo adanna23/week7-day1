2c153f265726475845773468d27cbb72e419da6d
Adanna <aydeeleje@gmail.com>
2021-04-28 17:00:57 -0400

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles.css';
+import { Home, Dashboard, SignIn } from './components';
+import reportWebVitals from './reportWebVitals';
+
+// import from react-router-Dom 
+import { BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Router>
+      <Switch>
+
+        <Route exact path = '/'>
+        <Home title={'Car World'} />
+        </Route>
+
+        <Route path = '/dashboard' component = {Dashboard} />
+
+        <Route path = '/signin' component = { SignIn } />
+        
+      </Switch>
+    </Router>
+   
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
